901cd559d9805ce5f1e330ba9a7f92b203040a05
JuanJoven01 <juanjov95@hotmail.com>
2024-04-14 18:16:37 -0500

    refactor to navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { BrowserRouter, useRoutes } from "react-router-dom"
+
 import {Home} from './pages/Home'
+import { Data } from './pages/Data'
 import './App.css'
+
+const AppRoutes = () => {
+
+  const routes = useRoutes(
+      [
+        {path: '/', element: <Home/> },
+        {path: '/data', element: <Data/> },
+      ]
+  )
+  return routes
+}
 
 function App() {
 
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/Data.jsx
@@ -0,0 +1,16 @@
+
+
+
+export const Data = () => {
+    return(
+        <section className=" h-screen font-montserrat">
+            Data
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </section>
+    )
+}
