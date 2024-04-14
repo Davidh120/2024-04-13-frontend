648905fb5e4416cf25c3ea122faaad2911c69f64
JuanJoven01 <juanjov95@hotmail.com>
2024-04-14 11:49:36 -0500

    initial config with assets, and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue1': '#00249c',
+        'blue2' : '#40cee4',
+        'grey1' : '#c5c5c5',
+        'red2' : '#e280be',
+        'red1' : '#c6007e'
+      },
+      fontFamily: {
+        'montserrat' : ["Montserrat", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
