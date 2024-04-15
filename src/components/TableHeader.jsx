9d1b1f483b0cae7ffb2db2f2c64f9ef89d398509
JuanJoven01 <juanjov95@hotmail.com>
2024-04-14 20:23:01 -0500

    created the header for the tables

--- src/components/TableHeader.jsx
+++ src/components/TableHeader.jsx
@@ -1,4 +1,12 @@
 
 
 
-c
+export const TableHeader = (props) => {
+
+
+    return(
+        <p className={`h-[63px] text-center font-montserrat text-[36.82px] font-[500] text-white bg-red2 w-[${props.width}px]`} >
+            {props.name}
+        </p>
+    )
+}
--- src/pages/Data.jsx
+++ src/pages/Data.jsx
@@ -5,13 +5,14 @@ import Location from '../assets/Icon_puntoubicacion.svg?react'
 import Person from '../assets/Icon_persona.svg?react'
 
 import { FormInput } from '../components/FormInput'
+import { TableHeader } from '../components/TableHeader'
 
 export const Data = () => {
     return(
         <section className=" h-screen font-montserrat">
             <div className="h-[85vh] flex">
                 <div>
-                    <div className=' relative h-[335px] w-[640px] shadow-[0px_4px_10px_0px_rgba(0,0,0,0.25);] rounded-[20px] left-[160px] top-[98px]'>
+                    <div className=' relative h-[335px] w-[640px] shadow-[0px_4px_10px_0px_rgba(0,0,0,0.25);] rounded-[20px] ml-[160px] mt-[98px]'>
                         <div className=' relative top-[13px] left-[28px]' >
                             <Plus />
                         </div>
@@ -20,13 +21,17 @@ export const Data = () => {
                             <FormInput name='Office' icon={<Location />} />
                             <FormInput name='Candidate' icon={<Person />} />
                         </div>
-                        
-
-
                     </div>
                 </div>
-                <div>
-                    To View
+                <div className='mt-[98px] w-full '>
+                    <div>
+                        <div className='flex justify-around px-16'>
+                            <TableHeader name='Marca' width={246} />
+                            <TableHeader name='Sucursal' width={246} />
+                            <TableHeader name='Aspirante' width={385} />
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
             <footer className='h-[15vh] flex justify-center items-center'>
