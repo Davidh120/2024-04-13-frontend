45f7104e831b326190a0ea6cdd053796567c5bcc
JuanJoven01 <juanjov95@hotmail.com>
2024-04-16 15:46:18 -0500

    homepage with the new styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,26 +12,29 @@ export const Home = () => {
             <div className='h-screen absolute top-0 left-0 w-screen'>
                 <Logo className='left-[58px] top-[43px] absolute ' />
 
-                <div className='top-0 h-[85vh] w-full absolute flex items-center justify-center' onClick={() =>  navigate('/data')}>
-                    <img className='' src={Cellphone} alt="Icon with a cellphone" />                    
+                <div className='top-0 h-[90vh] w-full absolute flex items-center justify-center hover:cursor-pointer ' onClick={() =>  navigate('/data')}>
+                    <img className='w-[667px] ' src={Cellphone} alt="Icon with a cellphone" />                    
                 </div>
 
-                <div className='top-0 h-[85vh] ' onClick={() =>  navigate('/data')}>
+                <div className='top-0 h-[90vh] hover:cursor-pointer ' onClick={() =>  navigate('/data')}>
                     <div className=' text-center font-bold text-blue1 h-full flex flex-col justify-center' >
-                        <h1 className=' text-[140px]'>
+                        <h1 className=' h-[140px] text-[140px]'>
                             BIENVENIDO A 
                         </h1>
-                        <h1 className=' text-[90px] z-10' style={{ textShadow: "-4px -4px 0 #fff, 4px -4px 0 #fff, -4px 4px 0 #fff, 4px 4px 0 #fff" }}>
+                        <h2 className='pl-[3px] h-[90px] text-[90px] z-10' style={{ textShadow: "-4px -4px 0 #fff, 4px -4px 0 #fff, -4px 4px 0 #fff, 4px 4px 0 #fff" }}>
                             MONITORING INNOVATION
-                        </h1>
+                        </h2>
+                        <p className="h-[70px]">
+
+                        </p>
                     </div>
                 </div>
 
-                <ul className='h-[15vh] flex justify-around text-blue2 text-[20px] font-medium'>
-                    <a href="https://monitoringinnovation.com/">MONITORING INNOVATION</a>
-                    <a href="https://gpscontrol.co/">GPS CONTROL</a>
-                    <a href="">Link repo front</a>
-                    <a href="">Link repo back</a>
+                <ul className='h-[10vh] flex justify-around text-blue2 text-[20px] font-medium'>
+                    <a href="https://monitoringinnovation.com/" target="_blank">MONITORING INNOVATION</a>
+                    <a href="https://gpscontrol.co/" target="_blank">GPS CONTROL</a>
+                    <a href="https://github.com/JuanJoven01/2024-04-13-frontend" target="_blank">Link repo front</a>
+                    <a href="https://github.com/JuanJoven01/2024-04-13-backend" target="_blank">Link repo back</a>
                 </ul>
             </div>
 
